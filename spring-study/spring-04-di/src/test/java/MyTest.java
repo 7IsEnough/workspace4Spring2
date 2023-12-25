680d7bf639f1392fca2ef6f1ed9b76ffdb1a2b99
Chen Zhenhuan <976949689@qq.com>
2023-12-25 23:38:46 +0800

    2023年12月25日23:38:41

--- spring-study/spring-04-di/src/test/java/MyTest.java
+++ spring-study/spring-04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.kobe.pojo.Student;
+import com.kobe.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,7 +14,18 @@ public class MyTest {
     ApplicationContext context = new ClassPathXmlApplicationContext(
         "beans.xml");
     Student student = (Student) context.getBean("student");
-    System.out.println(student.getName());
+    System.out.println(student);
+  }
+
+
+  @Test
+  public void test2() {
+    ApplicationContext context = new ClassPathXmlApplicationContext(
+        "userbeans.xml");
+    User user = context.getBean("user", User.class);
+    User user2 = context.getBean("user2", User.class);
+    System.out.println(user);
+    System.out.println(user2);
   }
 
 }
